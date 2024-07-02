3ae8662232c16c3d1acb83212526e5ff47ac57ff
Vitória Piloto <vitoriapiloto477@gmail.com>
2024-07-01 22:53:50 -0300

    Criando o get da API e configurando o entity

---
+++ Avaliação 2/WebAPI/WebApplication1/DataContext/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Model;
+
+namespace WebApplication1.DataContext
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Pedidos> pedidos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=NEXORJ73\SQLEXPRESS;Database=AvaliacaoAV1;Trusted_Connection=True;");
+
+            base.OnConfiguring(optionsBuilder);
+        }
+
+
+    }
+}
